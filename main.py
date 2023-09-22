28c3110dec523bdc1107eb2b0e8867ad6138f2c8
KevinED11 <mendez_kael@hotmail.com>
2023-09-22 08:44:42 -0700

    init

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
-def compare_objects(obj1: dict, obj2: dict) -> bool:
+from typing import TypeVar
+
+
+T = TypeVar("T")
+
+
+def compare_objects_with_identity_operator(obj1: T, obj2: T) -> bool:
+    """
+    Compare two objects and determine if they are the
+    same based on their memory address.
+    """
     return obj1 is obj2
 
 
-class Main(object):
+def compare_object_with_equality_operator(obj1: T, obj2: T) -> bool:
+    """
+    Compare two objects and determine if they are the
+    same based on their values.
+    """
+    return obj1 == obj2
+
+
+class Main:
     @staticmethod
     def main() -> None:
         print("Hello World!")
 
-        result = compare_objects({"a": 1, "b": 2}, {"a": 1, "b": 2})
+        result = compare_objects_with_identity_operator({"a": 1, "b": 2}, {"a": 1, "b": 2})
         print(result)
+
+        obj = {"a": 1, "b": 2}
+        obj2 = obj
+        result2 = compare_objects_with_identity_operator(obj, obj2)
+        print(result2)
+
+        obj3 = {"a": 1, "b": 2}
+        obj4 = {"a": 1, "b": 2}
+        result3 = compare_object_with_equality_operator(obj3, obj4)
+        print(result3)
 
 
 if __name__ == "__main__":
